0e2c4af61b30ec16a39d155aa87de7c39bd9d11c
sysoev-kirill <sysoev.kirill@gmail.com>
2023-08-05 08:37:39 +0300

    Fixed bug

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,15 +1,16 @@
 const allowedCors = [
 	'https://praktikum.tk',
 	'http://praktikum.tk',
-	'http://localhost:3000'
+	'http://localhost:3000',
+  'https://sysoev.nomoreparties.co'
 ];
 const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
 const cors = (req, res, next) => {
 	const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-	// проверяем, что источник запроса есть среди разрешённых 
+	// проверяем, что источник запроса есть среди разрешённых
 	const { method } = req;
-	const requestHeaders = req.headers['access-control-request-headers']; 
+	const requestHeaders = req.headers['access-control-request-headers'];
 	if (allowedCors.includes(origin)) {
 		res.header('Access-Control-Allow-Origin', origin);
 	}
