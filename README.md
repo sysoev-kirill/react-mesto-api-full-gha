feb86ffddba17eee201708a6be4558edbf37d6a3
sysoev-kirill <sysoev.kirill@gmail.com>
2023-08-05 13:25:26 +0300

    Fixed some bugs

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
   
 Пожалуйста, прикрепите в это описание ссылку на сайт, размещенный на Яндекс.Облаке.
 
-Адрес репозитория: https://github.com/...
+Адрес репозитория: https://github.com/sysoev-kirill/react-mesto-api-full-gha
 
 ## Ссылки на проект
 
-IP-адрес x.x.x.x
+IP-адрес 51.250.66.87
 
-Frontend https://...
+Frontend https://sysoev.nomoreparties.co
 
-Backend https://...
+Backend https://api.sysoev.nomoreparties.co
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,17 +2,18 @@ const allowedCors = [
   'https://sysoev.nomoreparties.co',
   'http://sysoev.nomoreparties.co',
   'http://localhost:3000',
+
 ];
 const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
 const cors = (req, res, next) => {
 	const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 	// проверяем, что источник запроса есть среди разрешённых
-  console.log(origin);
 	const { method } = req;
 	const requestHeaders = req.headers['access-control-request-headers'];
 	if (allowedCors.includes(origin)) {
 		res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
 	}
 	if (method === 'OPTIONS') {
 		// разрешаем кросс-доменные запросы с этими заголовками
